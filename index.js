d485fbc4f0ae824a2dde3c4f987f323ccc4d12cf
VI <ilmar.vodi@voco.ee>
2024-10-10 13:53:13 +0300

    andmete lugemine failist

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
-
 const path=require("path")
+const fs = require('node:fs')
+
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname, "views"))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    fs.readFile('./res/tasks.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    let tasks = data.split('\n')
+
+    res.render('index', {tasks:tasks})
+    });
 })
 
 app.listen(3001, () => {
