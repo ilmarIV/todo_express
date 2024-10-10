da2caeece1327c9592ff5656ef5971a4b6d42154
VI <ilmar.vodi@voco.ee>
2024-10-10 15:26:29 +0300

    faili kirjutamine

--- index.js
+++ index.js
@@ -3,20 +3,48 @@ const app = express()
 const path=require("path")
 const fs = require('node:fs')
 
+
+const readFiles = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const tasks = data.split('\n')
+            resolve(tasks)
+        });
+    })
+}
+
+
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
-    fs.readFile('./res/tasks.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    let tasks = data.split('\n')
-
+    readFiles('./res/tasks.txt').then((tasks) => {
     res.render('index', {tasks:tasks})
-    });
+    })
 })
+
+
+app.post('/', (req, res) => {
+    let task = req.body.task
+    readFiles('./res/tasks.txt').then((tasks) => {
+        tasks.push(task)
+        const data = tasks.join('\n')
+        fs.writeFile('./res/tasks.txt', data, err => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.redirect('/')
+        })
+    })
+})
+
 
 app.listen(3001, () => {
     console.log('app is started http://localhost:3001')
